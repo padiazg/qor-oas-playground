8607380d7c20328b5d78ee46449335ad5838700c
Patricio Diaz <padiazg@gmail.com>
2023-04-05 00:51:01 -0400

    WIP

--- app/api/api.go
+++ app/api/api.go
@@ -1,16 +1,11 @@
 package api
 
 import (
-	"path"
-
 	admin "github.com/TykTechnologies/portal-admin"
-	"github.com/TykTechnologies/portal-admin/oas"
 	"github.com/go-oas/docs"
 	"github.com/padiazg/qor-oas-playground/config/application"
 	"github.com/padiazg/qor-oas-playground/config/db"
-	tm "github.com/padiazg/qor-oas-playground/models/theme"
 	um "github.com/padiazg/qor-oas-playground/models/user"
-	"github.com/qor/roles"
 )
 
 // New new home app
@@ -68,59 +63,59 @@ func (app App) ConfigureApplication(application *application.Application) {
 	// accessRequests, _ := a.AddSubResource("AccessRequests")
 	// accessRequests.AddSubResource("Credentials")
 
-	themes := tm.Themes{
-		"theme1": &tm.Theme{
-			Name:    "theme1",
-			Version: "1.0.0",
-			Author:  "Tyk",
-			Path:    "./themes/theme1",
-		},
-		"theme2": &tm.Theme{
-			Name:    "theme2",
-			Version: "1.0.0",
-			Author:  "Tyk",
-			Path:    "./themes/theme2",
-		},
-	}
+	// themes := tm.Themes{
+	// 	"theme1": &tm.Theme{
+	// 		Name:    "theme1",
+	// 		Version: "1.0.0",
+	// 		Author:  "Tyk",
+	// 		Path:    "./themes/theme1",
+	// 	},
+	// 	"theme2": &tm.Theme{
+	// 		Name:    "theme2",
+	// 		Version: "1.0.0",
+	// 		Author:  "Tyk",
+	// 		Path:    "./themes/theme2",
+	// 	},
+	// }
 
-	z := API.AddResource(&tm.Config{}, &admin.Config{Name: "Theme"})
-	z.IndexAttrs("ID", "Name", "Status", "Author", "Version", "Path")
+	// z := API.AddResource(&tm.Config{}, &admin.Config{Name: "Theme"})
+	// z.IndexAttrs("ID", "Name", "Status", "Author", "Version", "Path")
 
-	z.FindManyHandler = themes.FindManyHandler
-	z.FindOneHandler = themes.FindOneHandler
+	// z.FindManyHandler = themes.FindManyHandler
+	// z.FindOneHandler = themes.FindOneHandler
 
-	z.RegisterRoute("GET",
-		path.Join(z.ParamIDName(), "download"),
-		tm.Download,
-		&admin.RouteConfig{PermissionMode: roles.Read},
-		oas.RouteConfig{
-			Method:   "GET",
-			Summary:  "Download a zip file",
-			AddToDoc: true,
-		},
-	)
+	// z.RegisterRoute("GET",
+	// 	path.Join(z.ParamIDName(), "download"),
+	// 	tm.Download,
+	// 	&admin.RouteConfig{PermissionMode: roles.Read},
+	// 	oas.RouteConfig{
+	// 		Method:   "GET",
+	// 		Summary:  "Download a zip file",
+	// 		AddToDoc: true,
+	// 	},
+	// )
 
-	z.RegisterRoute("PUT",
-		path.Join(z.ParamIDName(), "upload"),
-		tm.Upload,
-		&admin.RouteConfig{PermissionMode: roles.Update},
-		oas.RouteConfig{
-			Method:   "PUT",
-			Summary:  "Update a zip file",
-			AddToDoc: true,
-		},
-	)
+	// z.RegisterRoute("PUT",
+	// 	path.Join(z.ParamIDName(), "upload"),
+	// 	tm.Upload,
+	// 	&admin.RouteConfig{PermissionMode: roles.Update},
+	// 	oas.RouteConfig{
+	// 		Method:   "PUT",
+	// 		Summary:  "Update a zip file",
+	// 		AddToDoc: true,
+	// 	},
+	// )
 
-	z.RegisterRoute("POST",
-		path.Join("upload"),
-		tm.Upload,
-		&admin.RouteConfig{PermissionMode: roles.Create},
-		oas.RouteConfig{
-			Method:   "POST",
-			Summary:  "Upload a zip file",
-			AddToDoc: true,
-		},
-	)
+	// z.RegisterRoute("POST",
+	// 	path.Join("upload"),
+	// 	tm.Upload,
+	// 	&admin.RouteConfig{PermissionMode: roles.Create},
+	// 	oas.RouteConfig{
+	// 		Method:   "POST",
+	// 		Summary:  "Upload a zip file",
+	// 		AddToDoc: true,
+	// 	},
+	// )
 
 	application.Router.Mount(app.Config.Prefix, API.NewServeMux(app.Config.Prefix))
 
